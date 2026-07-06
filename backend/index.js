3c21121cf91f11d2e0ada4f3edba6ab942019756
0W3N <edoborosulagabriel01641564@gmail.com>
2026-07-06 19:45:55 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'http://localhost:3000',              // local frontend
-    'http://127.0.0.1:3000',              // optional
+    'https://spendwise-k80g.onrender.com',              // local frontend           // optional
     "https://spendwise-beryl-delta.vercel.app"  // deployed frontend
   ],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
