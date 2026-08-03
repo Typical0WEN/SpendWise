35030899e074042de31d33bbfc6f2b90defa2d70
0W3N <edoborosulagabriel01641564@gmail.com>
2026-08-03 17:43:58 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://spend-pilot0w3n.vercel.app" 
+    "https://spend-pilot0w3n.vercel.app",
     "https://spend-wise-eight-mu.vercel.app" // deployed frontend
   ],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
